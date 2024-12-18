93e324017d8cc94000cdfa47515cd16300eaeb5c
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-18 14:34:10 +0530

    add bloc listner

--- lib/counter/counter_page.dart
+++ lib/counter/counter_page.dart
@@ -10,7 +10,19 @@ class CounterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => CounterCubit(0),
-      child: CounterView(),
+      child: BlocListener<CounterCubit, int>(
+        listener: (context, state) {
+          if (state == 10) {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                content: Text("10 reached! "),
+              ),
+            );
+          }
+        },
+        child: const CounterView(),
+      ),
     );
   }
 }
