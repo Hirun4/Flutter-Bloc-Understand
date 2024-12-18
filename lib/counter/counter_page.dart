26c72d8706a3b23a476bd8edbf26df665c1533bf
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-18 14:07:19 +0530

    counter view

--- lib/counter/counter_page.dart
+++ lib/counter/counter_page.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_understand/counter/counter_cubit.dart';
+import 'package:bloc_understand/counter/counter_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
