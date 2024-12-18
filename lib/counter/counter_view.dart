db317f3c069dd480a9506c9a6f737322e6ca161c
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-18 14:12:32 +0530

    counter view

--- lib/counter/counter_view.dart
+++ lib/counter/counter_view.dart
@@ -1,10 +1,16 @@
+import 'package:bloc_understand/counter/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterView extends StatelessWidget {
   const CounterView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: BlocBuilder<CounterCubit, int>(
+      builder: (context, state) {
+        return Center(child: Text(state.toString()));
+      },
+    ));
   }
 }
